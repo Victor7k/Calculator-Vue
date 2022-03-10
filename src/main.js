89e83ca4ca40fb1a0cb3843525c79f40285b47f3
Victor <victor22158403@hotmail.com>
2022-03-09 23:28:03 -0300

    Meu comentário bonito

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+new Vue({
+    render: h => h(App) // Vai renderizar um componente Vue, dentro da div, e a partir desse componente raiz(App), todos os componentes da sua aplicação serão filhos dele.
+}).$mount("#app") // Esse é o padrão div usado quando se cria uma aplicação.
---
+++ src/main/CalculatorContent.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="calculator">
+        <Display :value="displayValue" />
+        <Button label="AC" triple @onClick="clearMemory" />
+        <Button label="/" operation @onClick="setOperation" />
+        <Button label="7" @onClick="addDigit" />
+        <Button label="8" @onClick="addDigit" />
+        <Button label="9" @onClick="addDigit" />
+        <Button label="*" operation @onClick="setOperation" />
+        <Button label="4" @onClick="addDigit" />
+        <Button label="5" @onClick="addDigit" />
+        <Button label="6" @onClick="addDigit" />
+        <Button label="-" operation @onClick="setOperation" />
+        <Button label="1" @onClick="addDigit" />
+        <Button label="2" @onClick="addDigit" />
+        <Button label="3" @onClick="addDigit" />
+        <Button label="+" operation @onClick="setOperation" />
+        <Button label="0" double @onClick="addDigit" />
+        <Button label="." @onClick="addDigit" />
+        <Button label="=" operation @onClick="setOperation" />
+    </div>
+</template>
+
+<script>
+    import Button from '../components/ButtonCalc.vue'
+    import Display from '../components/DisplayView.vue'
+
+    export default {
+        data: function() {
+            return {
+                displayValue: "0",
+                clearDisplay: false,
+                operation: null,
+                values: [0, 0],
+                current: 0
+            }
+        },
+        components: { Button, Display },
+        methods: {
+            clearMemory() {
+                // console.log('Limpar Memória!')
+                Object.assign(this.$data, this.$options.data()) // 'this.$options.data()' tem o estado inicial do objeto.
+            },
+            setOperation(operation) {
+                // console.log('Operação ' + operation)
+                if (this.current === 0) {
+                    this.operation === operation
+                    this.current = 1
+                    this.clearDisplay = true
+                } else {
+                    const equals = operation === '='
+                    const currentOperation = this.operation
+
+                    try {
+                        this.values[0] = eval(
+                            `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                        )
+                        if (isNaN(this.values[0]) || !isFinite(this.values[0])) {
+                            this.clearMemory()
+                            return
+                        }
+                    } catch (e) {
+                        this.$emit('onError', e)
+                    }
+
+                    this.values[1] = 0
+
+                    this.displayValue = this.values[0]
+                    this.operation = equals ? null : operation
+                    this.current = equals ? 0 : 1
+                    this.clearDisplay = !equals
+                }
+            },
+            addDigit(n) {
+                // console.log('Digito ' + n)
+                if (n === '.' && this.displayValue.includes('.')) {
+                    return 
+                }
+
+                const clearDisplay = this.displayValue === '0' 
+                    || this.clearDisplay
+                const currentValue = clearDisplay ? "" : this.displayValue
+                const displayValue = currentValue + n // Este é uma constante dentro da função.
+
+                // Este está acessando o 'displayValue' que fica fora do escopo.
+                this.displayValue = displayValue
+                this.clearDisplay = false // É usado quando o usuario digita um novo valor.
+                // Alternativa 1
+                this.values[this.current] = displayValue
+                
+                /* Alternativa 2 
+                    if(n !== '.') {
+                    const i = this.current
+                    const newValue = parseFloat(displayValue)
+                    this.values[i] = newValue
+                    }
+                */
+            }
+        }
+    }
+</script>
+
+<style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+    }
+</style>

400fd4d1d1ae829433bf85ed951e1ec8c2505770
Victor <victor22158403@hotmail.com>
2022-03-31 00:05:55 -0300

    atualizado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import Vue from 'vue'
-import App from './App'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-new Vue({
-    render: h => h(App) // Vai renderizar um componente Vue, dentro da div, e a partir desse componente raiz(App), todos os componentes da sua aplicação serão filhos dele.
-}).$mount("#app") // Esse é o padrão div usado quando se cria uma aplicação.
+// Vai renderizar um componente Vue, dentro da div, e a partir desse componente raiz(App), todos os componentes da sua aplicação serão filhos dele.
+createApp(App).mount("#app")
